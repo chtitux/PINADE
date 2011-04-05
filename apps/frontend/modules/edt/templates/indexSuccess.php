2725693a4fbdcd4fe1074e4abb2f1d47067e9675
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-04-05 21:06:09 +0200

    Retour vers la version avec helper

--- apps/frontend/modules/edt/templates/indexSuccess.php
+++ apps/frontend/modules/edt/templates/indexSuccess.php
@@ -17,24 +17,8 @@
 <?php endforeach ?>
 </ul>
 
-<div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId  : '145018112230561',
-      status : true, // check login status
-      cookie : true, // enable cookies to allow the server to access the session
-      xfbml  : true  // parse XFBML
-    });
-  };
-
-  (function() {
-    var e = document.createElement('script');
-    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-    e.async = true;
-    document.getElementById('fb-root').appendChild(e);
-  }());
-</script>
+<?php use_helper('sfFacebookConnect')?>
+    <?php echo include_facebook_connect_script() ?>
 
 
       <fb:login-button>Login with Facebook</fb:login-button>
