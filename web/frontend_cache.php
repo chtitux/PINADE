b7863acc466eb003053637401d6e42f23545c0fc
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-10-26 11:40:43 +0200

    Add the big switch in the frontend cache

--- web/frontend_cache.php
+++ web/frontend_cache.php
@@ -1,9 +1,12 @@
 <?php
 // web/frontend_cache.php
 
+require_once(dirname(__FILE__).'/switch-pool.php');
+
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '62.147.141.164')) && !(strpos($_SERVER['REMOTE_ADDR'], '2a01:e35:8aca:4470')!== false))
 {
-  die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//  die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
  
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
